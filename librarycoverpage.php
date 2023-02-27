760f705b3b630f21f90a84d558ef3b8e6c3275df
Rabby 5264 <nuruddinrabby83@gmail.com>
2023-02-27 11:28:51 +0600

    done

--- librarycoverpage.php
+++ librarycoverpage.php
@@ -1,7 +1,11 @@
 <?php
+include "inc/header.php";
 include "inc/sidenavbar.php";
-?>
 
+?>
+<video autoplay muted loop id="myVideo">
+        <source src="pictures/video.mp4" type="video/mp4">
+</video>
 <main>
     <div class="site-section">
         <div class="container">
