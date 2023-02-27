f9651b1277e3003bdf917d617ac0d4337bf1dcc5
SheikSalauddinBiswas <salauddinbiswas7@gmail.com>
2023-02-27 11:13:21 +0600

    new update

--- inc/sidenavbar.php
+++ inc/sidenavbar.php
@@ -39,31 +39,15 @@
           <span class="country">New York, USA</span>
         </div>
 
-        <div class="counter d-flex justify-content-center">
-          <!-- <div class="row justify-content-center"> -->
-            <div class="col">
-              <strong class="number">892</strong>
-              <span class="number-label">Posts</span>
-            </div>
-            <div class="col">
-              <strong class="number">22.5k</strong>
-              <span class="number-label">Followers</span>
-            </div>
-            <div class="col">
-              <strong class="number">150</strong>
-              <span class="number-label">Following</span>
-            </div>
-          <!-- </div> -->
-        </div>
+        
         
         <div class="nav-menu">
           <ul>
-            <li class="active"><a href="#"><span class="icon-home mr-3"></span>Feed</a></li>
+            <li class="active"><a href="index.php"><span class="icon-home mr-3"></span>HOME</a></li>
             <li><a href="#"><span class="icon-search2 mr-3"></span>Explore</a></li>
             <li><a href="#"><span class="icon-notifications mr-3"></span>Notifications</a></li>
-            <li><a href="#"><span class="icon-location-arrow mr-3"></span>Direct</a></li>
-            <li><a href="#"><span class="icon-pie-chart mr-3"></span>Stats</a></li>
-            <li><a href="#"><span class="icon-sign-out mr-3"></span>Sign out</a></li>
+            <li><a href="librarycoverpage.php"><span class="icon-pie-chart mr-3"></span>LIBRARY COVER PAGE</a></li>
+            <li><a href="signout.php"><span class="icon-sign-out mr-3"></span>Sign out</a></li>
           </ul>
         </div>
       </div>
--- makecoverpage.php
+++ makecoverpage.php
@@ -2,7 +2,36 @@
 include "inc/sidenavbar.php";
 ?>
 
+<main>
+    <div class="site-section">
+        <div class="container">
+            <div class="row justify-content-center">
 
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card">
+                            <img src="pictures/java book.jpeg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">programming in java</h5>
+                                <p class="card-text">66651 Programming in Java. T P C . 2 3 3. OBJECTIVES. • To develop knowledge and skill on programming Basics in Java Language.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+</main>
 
 
 
